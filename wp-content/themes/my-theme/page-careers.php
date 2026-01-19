e832754c6ad5a107fc6c292b70689d6f4fc729ba
nita <chheunnita169@gmail.com>
2026-01-19 09:36:27 +0700

    test plugin

---
+++ wp-content/themes/my-theme/page-careers.php
@@ -0,0 +1,33 @@
+
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/careers.css">
+<?php get_header();?>
+    <div class="header">
+        <div class="path">
+            <a href="#">Home</a>  <span>/ Careers</span>
+        </div>
+        <h1 class="title">Careers</h1>
+    </div>
+   
+    <div class="careers-section">
+        <div class="careers-content">
+            <h1>Careers</h1>
+
+           <h4>Join Almana Hospitals for a rewarding career in healthcare.</h4>
+
+            <p >
+            We value excellence and innovation. Explore our vacancies and
+            apply online. Together, we deliver exceptional healthcare and
+            improve lives. Join us today. Send your CV to:
+            </p>
+
+            <a href=""> </a>
+        </div>
+
+        <div class="careers-image">
+            <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/asset/hospital.jpg' ); ?>" alt="Director">
+
+        </div>
+</div>
+
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/my-theme/page-find-a-doctor copy.php
@@ -0,0 +1,181 @@
+<?php get_header();?>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+
+<div class="container">
+    <div class="header">
+      <div class="path">
+        <a href="#">Home</a>  <span>/ Find A Doctor</span>
+      </div>
+      <h1 class="title">Find A Doctor</h1>
+    </div>
+
+
+    <div class="about-container">
+        <div class="about-content">
+          <h3>At Almana Hospital's</h3>
+
+          <p>
+            We've proudly served our patients for over 75 years!. As a leader in Saudi
+            Arabia's healthcare landscape, we're dedicated to providing exceptional care
+            to our patients.
+          </p>
+
+          <h4>Find Your Doctor</h4>
+
+          <p>
+            Building trusting relationships with our patients is paramount.
+            Explore our extensive network of over 900 doctors and find the one who best
+            suits your needs.
+            <br />
+            <strong>Search now!</strong> by location, or specialty.
+          </p>
+        </div>
+     </div>
+
+     <section class="doctor-section">
+            <div class="doctor-grid">
+
+              <div class="doctor-card">
+                <div class="doctor-img">
+                  <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/asset/docter4.jpg' ); ?>" alt="Director">
+
+                </div>
+                <h3>Dr. Hean Sokhom Rathneary​</h3>
+                <p>General Practitioner & Aesthetics</p>
+                <button class="btn-profile">PROFILE</button>
+              </div>
+
+              <div class="doctor-card">
+                <div class="doctor-img">
+                    <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/asset/docter4.jpg' ); ?>" alt="Director">
+
+                </div>
+                <h3>Dr. Hean Sokhom Rathneary​</h3>
+                <p>General Practitioner & Aesthetics</p>
+                <button class="btn-profile">PROFILE</button>
+              </div>
+
+              <div class="doctor-card">
+                <div class="doctor-img">
+                  <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/asset/docter4.jpg' ); ?>" alt="Director">
+
+                </div>
+                <h3>Dr. Hean Sokhom Rathneary​</h3>
+                <p>General Practitioner & Aesthetics</p>
+                <button class="btn-profile">PROFILE</button>
+              </div>
+
+            </div>
+      </section>
+
+       <section class="doctor-section">
+            <div class="doctor-grid">
+
+              <div class="doctor-card">
+                <div class="doctor-img">
+                  <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/asset/docter4.jpg' ); ?>" alt="Director">
+
+                </div>
+                <h3>Dr. Hean Sokhom Rathneary​</h3>
+                <p>General Practitioner & Aesthetics</p>
+                <button class="btn-profile">PROFILE</button>
+              </div>
+
+              <div class="doctor-card">
+                <div class="doctor-img">
+                    <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/asset/docter4.jpg' ); ?>" alt="Director">
+
+                </div>
+                <h3>Dr. Hean Sokhom Rathneary​</h3>
+                <p>General Practitioner & Aesthetics</p>
+                <button class="btn-profile">PROFILE</button>
+              </div>
+
+              <div class="doctor-card">
+                <div class="doctor-img">
+                  <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/asset/docter4.jpg' ); ?>" alt="Director">
+
+                </div>
+                <h3>Dr. Hean Sokhom Rathneary​</h3>
+                <p>General Practitioner & Aesthetics</p>
+                <button class="btn-profile">PROFILE</button>
+              </div>
+
+            </div>
+      </section>
+       <section class="doctor-section">
+            <div class="doctor-grid">
+
+              <div class="doctor-card">
+                <div class="doctor-img">
+                  <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/asset/docter4.jpg' ); ?>" alt="Director">
+
+                </div>
+                <h3>Dr. Hean Sokhom Rathneary​</h3>
+                <p>General Practitioner & Aesthetics</p>
+                <button class="btn-profile">PROFILE</button>
+              </div>
+
+              <div class="doctor-card">
+                <div class="doctor-img">
+                    <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/asset/docter4.jpg' ); ?>" alt="Director">
+
+                </div>
+                <h3>Dr. Hean Sokhom Rathneary​</h3>
+                <p>General Practitioner & Aesthetics</p>
+                <button class="btn-profile">PROFILE</button>
+              </div>
+
+              <div class="doctor-card">
+                <div class="doctor-img">
+                  <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/asset/docter4.jpg' ); ?>" alt="Director">
+
+                </div>
+                <h3>Dr. Hean Sokhom Rathneary​</h3>
+                <p>General Practitioner & Aesthetics</p>
+                <button class="btn-profile">PROFILE</button>
+              </div>
+
+            </div>
+      </section>
+       <section class="doctor-section">
+            <div class="doctor-grid">
+
+              <div class="doctor-card">
+                <div class="doctor-img">
+                  <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/asset/docter4.jpg' ); ?>" alt="Director">
+
+                </div>
+                <h3>Dr. Hean Sokhom Rathneary​</h3>
+                <p>General Practitioner & Aesthetics</p>
+                <button class="btn-profile">PROFILE</button>
+              </div>
+
+              <div class="doctor-card">
+                <div class="doctor-img">
+                    <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/asset/docter4.jpg' ); ?>" alt="Director">
+
+                </div>
+                <h3>Dr. Hean Sokhom Rathneary​</h3>
+                <p>General Practitioner & Aesthetics</p>
+                <button class="btn-profile">PROFILE</button>
+              </div>
+
+              <div class="doctor-card">
+                <div class="doctor-img">
+                  <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/asset/docter4.jpg' ); ?>" alt="Director">
+
+                </div>
+                <h3>Dr. Hean Sokhom Rathneary​</h3>
+                <p>General Practitioner & Aesthetics</p>
+                <button class="btn-profile">PROFILE</button>
+              </div>
+
+            </div>
+      </section>
+
+</div>
+
+
+
+ <?php get_footer(); ?>
