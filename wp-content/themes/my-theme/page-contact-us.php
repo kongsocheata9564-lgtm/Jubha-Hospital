3768a9a2e0f827c7d82b0b5abb06c45aa0589bdd
Your Name <your_email@example.com>
2026-01-20 07:39:01 +0700

    connect link contact page

--- wp-content/themes/my-theme/page-contact-us.php
+++ wp-content/themes/my-theme/page-contact-us.php
@@ -1,12 +1,19 @@
+<?php get_header(); ?>
 <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/contact.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">  
 
-<?php get_header(); ?>
 
-     <div class=" contact-us">
-        <p> <span >Home </span>/ Contact Us</p>
-        <h1> Contact Us</h1>
-     </div>
+
+    <section class="page-header-section">
+    <div class="container">
+        <div class="breadcrumb">
+            <a href="http://jubha-hospital.test/" class="breadcrumb-link">HOME</a>
+            <span class="breadcrumb-separator">/</span>
+            <span class="breadcrumb-current">CONTACT US</span>
+        </div>
+        <h1 class="page-title">CONTACT US</h1>
+    </div>
+</section>
 
      
     <section class="contact-section">
@@ -60,7 +67,7 @@
     <div class="contact-box">
     
         <div class="left-info">
-            <div class="doctor-img">
+            <div class="doctor-pic">
                 <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/asset/images/Doctors Day Blog.jpg' ); ?>" alt="Director">
 
 
@@ -102,3 +109,32 @@
   <div class="location">
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31273.2101866894!2d104.8470770108398!3d11.541004200000016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31095183540eca35%3A0xbe81b4dcb0be7468!2sEcoDent%20Dental%20Clinic!5e0!3m2!1sen!2skh!4v1766717062822!5m2!1sen!2skh" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>          
   </div>
+
+
+  <section class="connect-subscribe">
+    <div class="container">
+
+        <!-- Left: Social Media -->
+        <div class="connect">
+            <h3>CONNECT WITH US</h3>
+            <div class="social-icons">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                <a href="#"><i class="fab fa-telegram-plane"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+            </div>
+        </div>
+
+        <!-- Right: Newsletter -->
+        <div class="subscribe">
+            <h3>SUBSCRIBE OUR NEWSLETTER</h3>
+            <form class="subscribe-form" method="post">
+                <input type="email" placeholder="Enter Your Email..." required>
+                <button type="submit">Subscribe</button>
+            </form>
+        </div>
+
+    </div>
+</section>
+
+  <?php get_footer(); ?>
