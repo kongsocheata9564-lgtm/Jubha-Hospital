5aef1d26cee2e14de027110dfe73c779ef4761b1
Your Name <your_email@example.com>
2026-01-20 08:35:01 +0700

    change careers

--- wp-content/themes/my-theme/page-careers.php
+++ wp-content/themes/my-theme/page-careers.php
@@ -61,33 +61,3 @@
 
 <?php get_footer(); ?>
 
-<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/careers.css">
-<?php get_header();?>
-    <div class="header">
-        <div class="path">
-            <a href="#">Home</a>  <span>/ Careers</span>
-        </div>
-        <h1 class="title">Careers</h1>
-    </div>
-   <section class="careers-section">
-        <div class="careers-content">
-            <h1>Careers</h1>
-            <div class="subtitle"> <h5>Join Almana Hospitals for a rewarding career in healthcare.</h5></div>
-            <p>
-            We value excellence and innovation. Explore our vacancies and apply online. Together, we deliver exceptional healthcare and improve lives.
-            </p>
-            <p>
-            Join us today. Send your CV to:<br />
-            <a href="mailto:career@almanahospital.com.sa">jubha Hospital@jubhahospital.com</a>
-            </p>
-        </div>
-
-
-        <div class="careers-image">
-           <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/asset/hospital.jpg' ); ?>" alt="Director">
-
-        </div>
-</section>
-      
-
-<?php get_footer(); ?>
